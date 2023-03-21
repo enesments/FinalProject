daba445749ecd3ec8191d4c7d8b1f025433fb96c
enesments <enesments@gmail.com>
2023-03-21 22:59:48 +0300

    Core

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,5 +1,4 @@
-﻿using Entities.Abstract;
-using Entities.Concrete;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,11 +7,11 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess //This pattern not reference other patterns
 {
     //generic constraint
     //class : referans type
-    //IEntity : IEntity or IEntity implements
+    //IEntity : IEntity or IEntity implements 
     //new() : It has to be new 
     public interface IEntityRepository<T> where T : class,IEntity,new()
     {
